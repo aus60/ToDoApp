44138c5fda66de19a7d27da4b79edabf4679ef03
Pavel Palancica <pavelpalancica@Pavels-MacBook-Pro.local>
2013-03-25 09:45:08 +0200

    Initial Commit

---
+++ ToDoApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ToDoApp
+//
+//  Created by Pavel Palancica on 3/25/13.
+//  Copyright (c) 2013 Pavel Palancica. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ToDoApp/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ToDoApp
+//
+//  Created by Pavel Palancica on 3/25/13.
+//  Copyright (c) 2013 Pavel Palancica. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
